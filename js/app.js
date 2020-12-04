2e2b5d4325cdf17cfc9d19ffead7fe725da52bf5
Datebayo350 <istrateconstantin6@gmaail.com>
2020-12-04 22:17:15 +0100

    Modify button works perfectly git add .!

--- js/app.js
+++ js/app.js
@@ -36,13 +36,10 @@ var todolist = {
         var divTitle = this.parentNode.parentNode.previousElementSibling;
         var inputs = divTitle.querySelectorAll('input');
         var paragraphs = divTitle.querySelectorAll('p');
-        var titleChildrens = divTitle.children;
         
         var input1Value = inputs[0].value = paragraphs[0].innerHTML;
         var input2Value = inputs[1].value = paragraphs[1].innerHTML;
 
-        var input1 = inputs[0];
-        var input2 = inputs[1];
 
         console.log(inputs);
         
@@ -59,12 +56,61 @@ var todolist = {
         
 
         );
-        inputs.forEach(input => input.addEventListener('blur', (event) => {
-            event.target.style.background = 'pink';    
-          }));
+            
+        /**
+         * Ajoute un évenement sur une liste
+         * 
+         * @param  list 
+         * @param  event 
+         */
+
+        var addBlurEventListener = function( list, event) {
+            for(var i = 0; i <list.length; i++) {
+                list[i].addEventListener(event, (evt) => {
+                   var field = evt.target.value;
+                    console.log(evt);
+                    if(evt.key === 'Enter' || evt.type === 'blur'){
+                      
+                      var paragraph1Value = paragraphs[0].innerHTML = inputs[0].value;
+                      var paragraph2Value = paragraphs[1].innerHTML = inputs[1].value;
+
+                      console.log(evt.key);
+                      console.log('Paragraphe 1 =>', paragraph1Value);
+                     
+                       
+                        inputs.forEach(input => 
+                                
+                            input.classList.add('d-none')
+
+                        );
+
+                        paragraphs.forEach(p => 
+                        
+                            p.classList.remove('d-none')
+                        
+
+                        );
+                      
+                    }
+
+                    
+                })
+
+            }
+        }
+
+        addBlurEventListener(inputs, 'blur')
+        addBlurEventListener(inputs, 'keydown')
+
+
+
+    },
+
+    
+
          
         
-    }
 }
+
 
 todolist.init();
