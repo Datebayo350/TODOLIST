4a2fb7e8b7b2dd725adbba42277507bc33e30f4c
Datebayo350 <istrateconstantin6@gmaail.com>
2020-12-06 04:56:40 +0100

    Code refactorized, posibility to edit the news displayed tasks from the submit form, rest to handle de progress bar

--- js/app.js
+++ js/app.js
@@ -1,26 +1,60 @@
 var todolist = {
 
     init: function(){
-        var checks = document.querySelectorAll('.check');
-        var delets = document.querySelectorAll('.delete');
-        var modifys = document.querySelectorAll('.modify');
-        var submitNewTaskButton = document.querySelector('button[type=submit');
-        var form = document.querySelector('form');
-        
-        checks.forEach(index => index.addEventListener('click', todolist.handleCheckButton));
-        delets.forEach(index => index.addEventListener('click', todolist.handleDeleteButton));
-        modifys.forEach(index => index.addEventListener('click', todolist.handleModifyButton));
+        // var submitNewTaskButton = document.querySelector('button[type=submit');
         // submitNewTaskButton.addEventListener('click', todolist.handleAddNewTask);
+
+        var form = document.querySelector('form');
         form.addEventListener('submit', todolist.handleAddNewTask);
+        todolist.getTheTask();
+    },
+    
+    getTheTask: function () {
         
+        // Recover the task container
+        var tasks = document.querySelectorAll('.parent__task');
         
+        for ( var i = 0; i < tasks.length; i++){
+            // Eache task
+            currentTask = tasks[i];
+
+            // We'll gona apply all the events on her
+            todolist.bindEventsToTheTask(currentTask);
+
+        }
+
+    },
+    
+    bindEventsToTheTask: function (task) {
         
+         // Her check button
+         checkButton = task.querySelector('.check');
+         // Her modify button
+         modifyButton = task.querySelector('.modify');
+         // Title, category on  click
+         paragraphs = task.querySelector('.task__paragraph');
+         // Her delete button
+         deleteButton = task.querySelector('.delete');
+
+         // We'll gonna apply an click event on each on of them
+         checkButton.addEventListener('click',todolist.handleCheckButton)
+         modifyButton.addEventListener('click', todolist.handleModifyButton);
+         paragraphs.addEventListener('dblclick', todolist.handleModifyButton);
+         deleteButton.addEventListener('click', todolist. handleDeleteButton);
+         
+         var inputs = task.querySelectorAll('input');
+        //  console.log(inputs);
+        
+        //We'll gonna handle the blur, and the press of the enter-key, for the inputs fields when they appear to edit the title / category
+        todolist.addEventListener(inputs, 'blur');
+        todolist.addEventListener(inputs, 'keydown');
+
     },
 
     handleCheckButton: function(event){
-       
+       // Recover the progress div
         var divProgress = event.target.parentNode.parentNode.nextElementSibling.childNodes[1];
-       
+       // Set its progress, we'll gonna handle it dinamically for the news one's
         divProgress.style.width = '100%';
         divProgress.textContent = '100%';
 
@@ -28,26 +62,53 @@ var todolist = {
 
     handleDeleteButton: function(event){
        
-        var divParent = event.target.closest('.parent');
-        var divAncestor = event.target.closest('.ancestor');
+        // var parentTasks = event.path[5];
+        // var parentTasks = this.closest('.parent__tasks');
+        
+        // Recover the delet button
+        var parentTasks = event.target.closest('.parent__tasks');
+        
+        // var divParent = event.path[4];
+        // Recover his parend div
+        var divParent = event.target.closest('.parent__task');
+        
+        console.log(divParent);
 
-        divAncestor.removeChild(divParent);
+        // Remove the task
+        parentTasks.removeChild(divParent);
 
     },
 
     handleModifyButton: function(event){
-        //? This refer to the currentTarget
-        var divTitle = event.target.parentNode.parentNode.previousElementSibling;
-        console.log(divTitle);
+        //? -This- refer to the currentTarget (event.target), button modify
+        //#1 
+        //var divTitle = event.target.parentNode.parentNode.previousElementSibling;
         
+        //#2
+        // Recover the parent__task div
+        var divTitle = event.path[3];
+
+        // Select all her inputs
         var inputs = divTitle.querySelectorAll('input');
+        // console.log('MES INPUTS', inputs[1]);
+        //! Clean the value of the inputs each time the event happend, it provides us to don't cumulates the values of the lasts editings
+        inputs.forEach(input=> input.value='')
         var paragraphs = divTitle.querySelectorAll('p');
+        // console.log('MES PARAGRAPHS', paragraphs);
+
         
         //?Set the value of the futur displayed inputs
-        var input1Value = inputs[0].value = paragraphs[0].innerHTML;
-        var input2Value = inputs[1].value = paragraphs[1].innerHTML;
+       // #1
+        for(var i = 0; i < inputs.length; i++){
+            inputs[i].value += paragraphs[i].innerHTML;
+        }
+        
+        // #2
+        // var input1Value = inputs[0].value = paragraphs[0].innerHTML;
+        // var input2Value = inputs[1].value = paragraphs[1].innerHTML;
 
         //? On Every Click to the modify button, hide the paragraphs and display the inputs to let me edit the field
+       
         inputs.forEach(input => 
                 
             input.classList.remove('d-none')
@@ -60,71 +121,82 @@ var todolist = {
         
 
         );
-        todolist.addBlurEventListener(inputs, 'blur')
-        todolist.addBlurEventListener(inputs, 'keydown')
-
+        console.log(event);
+        
     },
 
     handleAddNewTask: function(event){
         event.preventDefault();
         console.log(event);
-        var ancestor = document.querySelector('.ancestor');
+        var parentTasks = document.querySelector('.parent__tasks');
         var template = document.querySelector('template');
         
         var formTitle = document.querySelector('.formTitle_task');
         var formCategory = document.querySelector('.formCategory_task');
         var formProgress = document.querySelector('.formProgress_task');
         
+
         // @see https://stackoverflow.com/questions/39372886/document-importnode-vs-node-clonenode-real-example
         // @see https://developer.mozilla.org/fr/docs/Web/API/Node/cloneNode
-        //var clone = document.importClone(template.content, clone,true)
-        //? Select the template, get the content, clone it and tru to say that we want to clone his childrens too
-        //! This one its a document fragment
-        var cloneTemplateFragment = document.getElementById('template').content.cloneNode(true);
-        //! This one its a an element, so add this one and not the above one
-        var templateParent = cloneTemplateFragment.querySelector('.parent');
+        //? Select the template, get the content, clone it and 'true' to say that we want to clone his childrens too
+        var cloneTemplateFragment = document.importNode(template.content,true)
+        //! This one is a document fragment
+        // var cloneTemplateFragment = document.getElementById('template').content.cloneNode(true);
+        //! This one is a an element, so add this one on the flow, not the above one
+        var templateParent = cloneTemplateFragment.querySelector('.parent__task');
+       
+
         var templateTitle = cloneTemplateFragment.querySelector('.templateTitle_task');
         var templateCategory = cloneTemplateFragment.querySelector('.templateCategory_task');
         var templateProgress = cloneTemplateFragment.querySelector('.templateProgress_task');
 
         templateTitle.innerHTML = formTitle.value;
-        // console.log(templateParent);
         templateParent.classList.add('shadow-test'); 
         templateCategory.innerHTML = formCategory.value;
         templateProgress.innerHTML = formProgress.value;
         
         // @see https://stackoverflow.com/questions/11475232/find-the-element-before-and-after-a-specific-element-with-pure-javascript
         // @see https://stackoverflow.com/questions/618089/can-i-insert-elements-to-the-beginning-of-an-element-using-appendchild
-        ancestor.insertBefore(templateParent, ancestor.firstChild);
+        parentTasks.insertBefore(templateParent, parentTasks.firstChild);
         // progress.style.backgroundColor = "red";
         
-        console.log(templateParent);
-        
-        
+        //Call again the methode to give us the possibility to ediy the news tasks
+        todolist.bindEventsToTheTask(templateParent);
     },
     
-    //? Add event on every kind of array, object
-    addBlurEventListener: function(list, event){
-        var inputs = document.querySelectorAll('.task-input');
-        var paragraphs = document.querySelectorAll('.task-paragraph');
+    /**
+     * Add event on every kind of array, object
+     * @param {*} list 
+     * @param {*} event 
+     */
+    addEventListener: function(list, event){
+        var paragraphs = list[0].parentNode.querySelectorAll('p');
+        // Necesary even if its the same value like list, because in the loop we can't acces it anymore
+        var inputs = list[0].parentNode.querySelectorAll('input');
+        // console.log(list);
+        
+            
         for(var i = 0; i <list.length; i++) {
-            //?For every item in the list, add the listener
+            // console.log('item',list[i]);
+            // console.log('evt',paragraphs[i]);
+            
+            //For every input in the list, add the listener
             list[i].addEventListener(event, (evt) => {
+                
                 if(evt.key === 'Enter' || evt.type === 'blur'){
-                  
-                   paragraphs[0].innerHTML = inputs[0].value;
-                   paragraphs[1].innerHTML = inputs[1].value;
                     
-                    //? Do the oposit job of the l 44
-                    inputs.forEach(input => 
-                            
-                        input.classList.add('d-none')
-                    );
-
-                    paragraphs.forEach(p => 
+                    for(let i = 0; i < paragraphs.length; i++) {
+                        //? For tge paragraph situeted at the position i (number of the loop) in his nodeListe, the value will be the actual one of the input situed at the place i (number of the actual itération of the loop) in his nodeList
+                        paragraphs[i].innerHTML = inputs[i].value;
+                        paragraphs[i].classList.remove('d-none');
+                        inputs[i].classList.add('d-none');
+                        
+                        
+                    }
                     
-                        p.classList.remove('d-none')
-                    );
+                    
+                    
+        
                 }
             })
         }
